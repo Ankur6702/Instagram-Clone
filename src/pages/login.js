1c92692f7c36bc974113b1bc5cc8ab40391eb527
Ankur Agarwal <ankuragarwa246@gmail.com>
2021-09-11 01:09:53 +0530

    Updated tailwind config file for self defined styles

--- src/pages/login.js
+++ src/pages/login.js
@@ -27,7 +27,7 @@ export default function Login() {
                 <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
             </div>
             <div className="flex flex-col 2-2/5">
-                <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+                <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
                     <h1 className="flex justify-center w-full">
                         <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
                     </h1>
@@ -60,7 +60,7 @@ export default function Login() {
                         </button>
                     </form>
                 </div>
-                <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-primary-gray">
+                <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary rounded">
                     <p className="text-sm">Don't have an account?{` `}
                         <Link to="/signup" className="font-bold text-blue-medium">
                             Sign Up
